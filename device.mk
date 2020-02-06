68ada06c7d1db08215ea72b18b11772bd096c761
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-06 13:02:47 +0700

    lavender: Lineage-ify
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -14,9 +14,12 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS := \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 #    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Audio
 PRODUCT_PACKAGES += \
